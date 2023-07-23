9da43bcb63594d0c4765be39c3fc3273fde346fc
Inder-singh001 <106738356+Inder-singh001@users.noreply.github.com>
2023-07-23 18:59:58 +0530

    ui_responsive

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -33,48 +33,7 @@ class Myhomepage extends StatefulWidget {
 }
 
 class _MyhomepageState extends State<Myhomepage> with TickerProviderStateMixin {
-  // void setState(()){};
-  // final DecorationTween decorationTween = DecorationTween(
-  //     begin: BoxDecoration(
-  //       color: Color(0xFFFFFFFF),
-  //       border: Border.all(style: BorderStyle.none),
-  //       borderRadius: BorderRadius.circular(60.0),
-  //       boxShadow: const <BoxShadow>[
-  //         BoxShadow(
-  //           color: Color(0x66666666),
-  //           blurRadius: 10.0,
-  //           spreadRadius: 3.0,
-  //           offset: Offset(0, 6.0),
-  //         ),
-  //       ],
-  //     ),
-  //     end: BoxDecoration(
-  //       color: const Color(0xFFFFFFFF),
-  //       border: Border.all(
-  //         style: BorderStyle.none,
-  //       ),
-  //       borderRadius: BorderRadius.zero,
-  //     ));
-
-  // late final AnimationController _controller = AnimationController(
-  //   vsync: this,
-  //   duration: const Duration(seconds: 1),
-  // )..repeat(reverse: true);
-
-  // @override
-  // void dispose() {
-  //   _controller.dispose();
-  //   super.dispose();
-  // }
-
   double _size = 150;
-  // bool _large = false;
-  // void _updateSize() {
-  //   setState(() {
-  //     _size = _large ? 250 : 150;
-  //     _large = !_large;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
